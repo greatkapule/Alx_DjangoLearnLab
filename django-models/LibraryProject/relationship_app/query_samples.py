86f587f51ad86c959e492424948494e8b585bdc2
edwin kapule <greatkapule@gmail.com>
2026-01-20 03:46:39 +0300

    Wrap relationship queries inside functions as required by ALX checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,22 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 
-# Query all books by a specific author.
-author = Author.objects.get(name="Chinua Achebe")
-books = Book.objects.filter(author=author)
+def query_books_by_author():
+    # Query all books by a specific author.
+    author = Author.objects.get(name="Chinua Achebe")
+    books = Book.objects.filter(author=author)
+    return books
 
 
-# List all books in a library.
-library = Library.objects.get(name="City Library")
-books_in_library = library.books.all()
+def list_books_in_library():
+    # List all books in a library.
+    library = Library.objects.get(name="City Library")
+    books_in_library = library.books.all()
+    return books_in_library
 
 
-# Retrieve the librarian for a library.
-librarian = Librarian.objects.get(library=library)
+def get_librarian_for_library():
+    # Retrieve the librarian for a library.
+    library = Library.objects.get(name="City Library")
+    librarian = Librarian.objects.get(library=library)
+    return librarian
