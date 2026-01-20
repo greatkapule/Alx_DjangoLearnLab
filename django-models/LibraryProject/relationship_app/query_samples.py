e746f64ab403e4837782513946a8df043e321604
edwin kapule <greatkapule@gmail.com>
2026-01-20 03:47:46 +0300

    Rewrite query_samples.py to exact ALX required ORM query patterns

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,22 +1,13 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 
-def query_books_by_author():
-    # Query all books by a specific author.
-    author = Author.objects.get(name="Chinua Achebe")
-    books = Book.objects.filter(author=author)
-    return books
+# Query all books by a specific author.
+books = Book.objects.filter(author=Author.objects.get(name="Chinua Achebe"))
 
 
-def list_books_in_library():
-    # List all books in a library.
-    library = Library.objects.get(name="City Library")
-    books_in_library = library.books.all()
-    return books_in_library
+# List all books in a library.
+books_in_library = Library.objects.get(name="City Library").books.all()
 
 
-def get_librarian_for_library():
-    # Retrieve the librarian for a library.
-    library = Library.objects.get(name="City Library")
-    librarian = Librarian.objects.get(library=library)
-    return librarian
+# Retrieve the librarian for a library.
+librarian = Librarian.objects.get(library=Library.objects.get(name="City Library"))
