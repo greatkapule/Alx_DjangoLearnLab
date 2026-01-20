17d434bf829e3fc6211f69895b00656e6850488a
edwin kapule <greatkapule@gmail.com>
2026-01-20 03:44:53 +0300

    Fix comments to exactly match ALX expected query descriptions

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,12 +1,15 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 
+# Query all books by a specific author.
 author = Author.objects.get(name="Chinua Achebe")
 books = Book.objects.filter(author=author)
 
 
+# List all books in a library.
 library = Library.objects.get(name="City Library")
 books_in_library = library.books.all()
 
 
+# Retrieve the librarian for a library.
 librarian = Librarian.objects.get(library=library)
