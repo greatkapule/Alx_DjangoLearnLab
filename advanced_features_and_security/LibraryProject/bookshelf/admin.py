e5cdda4acd27bf0f9e97dffd17cde1decb345d52
edwin kapule <greatkapule@gmail.com>
2026-01-24 05:46:37 +0300

    Add custom user model with date_of_birth and profile_photo

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -4,14 +4,16 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
     fieldsets = UserAdmin.fieldsets + (
-        ("Additional Information", {
+        ("Additional Info", {
             "fields": ("date_of_birth", "profile_photo"),
         }),
     )
 
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Additional Information", {
+        ("Additional Info", {
             "fields": ("date_of_birth", "profile_photo"),
         }),
     )
