6dcd489b91fe784ef18fc5f9d3371098cc31619a
edwin kapule <greatkapule@gmail.com>
2026-01-25 17:40:11 +0300

    Add custom user model with date_of_birth and profile_photo

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,32 +1,30 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django import forms
 from .models import CustomUser
 
-# 1. Define the forms so the Admin knows your fields exist
-class CustomUserCreationForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ("username", "email", "date_of_birth", "profile_photo")
 
-class CustomUserChangeForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ("username", "email", "date_of_birth", "profile_photo")
-
-# 2. Update the Admin Class
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm  # Use our new 'Add' form
-    form = CustomUserChangeForm        # Use our new 'Edit' form
     model = CustomUser
 
-    # Fields to show when EDITING a user
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    list_display = (
+        "username",
+        "email",
+        "date_of_birth",
+        "is_staff",
+        "is_active",
     )
 
-    # Fields to show when CREATING a user
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
     )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
