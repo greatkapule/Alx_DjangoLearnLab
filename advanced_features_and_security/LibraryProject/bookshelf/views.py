3dd684e5e950db2159da8bf8a3c5e78e8fdcde8b
edwin kapule <greatkapule@gmail.com>
2026-01-25 19:57:00 +0300

    Implement permissions and groups for bookshelf app

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
-
+from .models import Book
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_books(request):
-    return render(request, 'view_books.html')
-
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
-    return render(request, 'create_book.html')
-
+    return render(request, 'bookshelf/form_example.html')
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, pk):
-    return render(request, 'edit_book.html')
-
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'bookshelf/form_example.html', {'book': book})
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request, pk):
-    return render(request, 'delete_book.html')
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'bookshelf/confirm_delete.html', {'book': book})
