c233eebf1742d52e4e59f161c530041c663fa09d
edwin kapule <greatkapule@gmail.com>
2026-02-14 05:22:26 +0300

    Add full CRUD functionality for Blog Posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,41 +1,55 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
+# View to list all posts
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/post_list.html'  # <app>/<model>_<viewtype>.html
+    template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     ordering = ['-published_date']
 
+# View to see a single post
 class PostDetailView(DetailView):
     model = Post
 
+# View to create a post - Required LoginRequiredMixin
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
+        # Automatically set the author to the logged-in user
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+# View to update a post - Required LoginRequiredMixin and UserPassesTestMixin
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
+        # Ensure the author remains the logged-in user
         form.instance.author = self.request.user
         return super().form_valid(form)
 
     def test_func(self):
+        # Logic to ensure only the author can edit
         post = self.get_object()
         return self.request.user == post.author
 
+# View to delete a post - Required LoginRequiredMixin and UserPassesTestMixin
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    success_url = '/'
+    success_url = '/'  # Redirect to home after deletion
 
     def test_func(self):
+        # Logic to ensure only the author can delete
         post = self.get_object()
         return self.request.user == post.author
