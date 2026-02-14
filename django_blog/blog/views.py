f9d88a3144c9cae75e50969b3a5a15ccd6a636dd
edwin kapule <greatkapule@gmail.com>
2026-02-14 07:28:13 +0300

    Final fix for LoginRequiredMixin and UserPassesTestMixin check

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -7,6 +7,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    ordering = ['-published_date']
 
 class PostDetailView(DetailView):
     model = Post
