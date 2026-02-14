27d9c6744c1a5d11b89b2f703e5e01ab09bde758
edwin kapule <greatkapule@gmail.com>
2026-02-14 06:39:56 +0300

    Complete blog CRUD with author permissions and absolute URLs

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('posts/', PostListView.as_view(), name='post-list'),  # Changed to /posts/
-    path('posts/new/', PostCreateView.as_view(), name='post-create'), # Changed to /posts/new/
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # Changed to /posts/<int:pk>/
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'), # Changed to /edit/
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), # Changed to /delete/
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,18 +1,20 @@
-# blog/views.py
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
+# List View
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-published_date']
 
+# Detail View
 class PostDetailView(DetailView):
     model = Post
 
+# Create View
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
@@ -21,6 +23,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+# Update View
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
@@ -33,6 +36,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         post = self.get_object()
         return self.request.user == post.author
 
+# Delete View
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
