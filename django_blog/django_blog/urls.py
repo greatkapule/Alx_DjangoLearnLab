38ac53a0cb54fe6ebccd0d4a198e50e20d3e8f4f
edwin kapule <greatkapule@gmail.com>
2026-02-14 06:14:57 +0300

    Implemented full CRUD blog post management with permissions

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),   # ← THIS LINE IS IMPORTANT
+    path('', include('blog.urls')),
 ]
